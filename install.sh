2001cae7c6e608e9fa70a6fa7f09ef97fcff160f
Miguel Amador <miguesdev@gmail.com>
2023-04-26 22:56:59 -0500

    feat: reload terminal session when the installation is over

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [ "$(echo $SHELL)" = "/bin/zsh" ]; then
     echo '';
     echo -e "laravel-aliases was added to ~/.zshrc";
     echo -e "\033[32mStart it on Github:\033[0m https://github.com/migueweb/laravel-aliases";
+    source ~/.zshrc
 
 elif [ "$(echo $SHELL)" = "/bin/bash" ]; then
     
@@ -13,6 +14,7 @@ elif [ "$(echo $SHELL)" = "/bin/bash" ]; then
     echo '';
     echo -e "laravel-aliases was added to ~/.bashrc";
     echo -e "\033[32mStart it on Github:\033[0m https://github.com/migueweb/laravel-aliases";
+    source ~/.bashrc
 
 else
   echo -e "\e[31mError:\e[0m Bash or Zsh required or check the requirements on https://github.com/migueweb/laravel-aliases";
